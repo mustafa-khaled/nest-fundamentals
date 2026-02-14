d47e575677f831d6a20853d0e39aa0df7e4438eb
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2026-02-14 12:59:34 +0200

    query params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Body, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import type { User } from './users.service';
 
 @Controller('users')
 export class UsersController {
@@ -7,6 +8,24 @@ export class UsersController {
 
   @Get()
   getUsers() {
-    return this.userService.getUsers();
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.userService.getUserById(Number(id));
+  }
+
+  @Get(':id/:gender')
+  getUserByIdAndGender(@Param() params: { id: string; gender: string }) {
+    return this.userService.getUserByIdAndGender(
+      Number(params.id),
+      params.gender,
+    );
+  }
+
+  @Post()
+  createUser(@Body() user: User) {
+    return this.userService.createUser(user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,55 @@
 import { Injectable } from '@nestjs/common';
 
+export interface User {
+  id: number;
+  name: string;
+  age: number;
+  gender: string;
+  isMarried: boolean;
+}
+
 @Injectable()
 export class UsersService {
   private users = [
     {
       id: 1,
       name: 'Mustafa',
+      age: 24,
+      gender: 'Male',
+      isMarried: false,
     },
     {
       id: 2,
+      name: 'Mariam',
+      age: 25,
+      gender: 'Female',
+      isMarried: true,
+    },
+    {
+      id: 3,
       name: 'Khaled',
+      age: 25,
+      gender: 'Male',
+      isMarried: true,
     },
   ];
 
-  getUsers() {
+  getAllUsers() {
     return this.users;
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  getUserByIdAndGender(id: number, gender: string) {
+    return this.users.find(
+      (user) => user.id === id && user.gender === gender,
+    );
+  }
+
+  createUser(user: User) {
+    this.users.push(user);
+    return user;
   }
 }
