7d91dc8f8aa393a090cc62f1aee5d2bb28dbb9ae
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2026-02-17 21:18:44 +0200

    Inter Modular Dependency

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -1,13 +1,23 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { TweetService } from './tweet.service';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('tweet')
 export class TweetController {
   constructor(private readonly tweetService: TweetService) {}
 
   @Get()
-  @Get(':userId')
-  getTweets(@Param('userId') userId?: string) {
+  getTweets() {
     return this.tweetService.getTweets();
+  }
+
+  @Get(':tweetId')
+  getTweetById(@Param('tweetId', ParseIntPipe) tweetId: number) {
+    return this.tweetService.getTweetById(tweetId);
+  }
+
+  @Get('user/:userId')
+  getTweetsByUserId(@Param('userId', ParseIntPipe) userId: number) {
+    return this.tweetService.getTweetsByUserId(userId);
   }
 }
--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class TweetService {
+  constructor(private readonly userService: UsersService) {}
   private readonly tweets = [
     {
       id: 1,
       content: 'Hello tweets',
+      userId: 1,
     },
     {
       id: 2,
       content: 'Hello tweets 2',
+      userId: 2,
     },
   ];
+
   getTweets() {
     return this.tweets;
+  }
+
+  getTweetById(id: number) {
+    return this.tweets.find((tweet) => tweet.id === id);
+  }
+
+  getTweetsByUserId(userId: number) {
+    const user = this.userService.getUserById(userId);
+    const tweets = this.tweets.filter((tweet) => tweet.userId === userId);
+    return {
+      user,
+      tweets,
+    };
   }
 }
