a1869465a276bf02beb478a7e3cefa2bdb9b7695
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2026-02-21 13:13:57 +0200

    connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,30 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, TweetModule, AuthModule],
+  imports: [
+    UsersModule,
+    TweetModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'mustafakhaled',
+        password: 'password',
+        database: 'nestjs',
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
